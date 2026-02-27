64927938583c8b3d1c583bf80d509bbc3e373e0e
TianRosandhy <christian.rosandhy@pos.com.my>
2026-02-27 14:44:19 +0800

    fix: Validate author field as mandatory

--- src/dto/book_request.go
+++ src/dto/book_request.go
@@ -2,7 +2,7 @@ package dto
 
 type BookRequest struct {
 	Title  string `json:"title" validate:"required,max=200"`
-	Author string `json:"author" validate:"omitempty,max=200"`
+	Author string `json:"author" validate:"required,max=200"`
 	Year   int    `json:"year"`
 }
 
